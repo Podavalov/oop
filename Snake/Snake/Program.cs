f368622116d8cb4e32d6f96db32a9c37893ae086
Gleb <podavalovg@gmail.com>
2021-07-19 04:33:53 +0500

    step 1

--- Snake/Snake/Program.cs
+++ Snake/Snake/Program.cs
@@ -1,4 +1,8 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
 
 namespace Snake
 {
@@ -6,7 +10,22 @@ namespace Snake
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Point p1 = new Point();
+
+                p1.x = 2;
+                p1.y = 4;
+                p1.sym = '*';
+                p1.Draw();
+            Point p2 = new Point();
+            p2.x = 4;
+            p2.y = 8;
+            p2.sym = '#';
+            p2.Draw();
+             
+
+            Console.ReadLine();
         }
+
+       
     }
 }
