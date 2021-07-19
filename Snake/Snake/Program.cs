9c89776e27992fa531344b482943f8821def9ab9
Gleb <podavalovg@gmail.com>
2021-07-19 05:35:00 +0500

    step4

--- Snake/Snake/Program.cs
+++ Snake/Snake/Program.cs
@@ -13,27 +13,11 @@ namespace Snake
             Point p1 = new Point(1, 3, '*');
             p1.Draw(); 
            
-             Point p2 = new Point(5, 2, '#');
+             Point p2 = new Point(2, 3, '#');
                    p2.Draw();
 
-            List<int> numlist = new List<int>();
-            numlist.Add(0);
-            numlist.Add(1);
-            numlist.Add(2);
-
-            int x = numlist[0];
-            int y = numlist[1];
-            int z = numlist[2];
-
-            foreach (int i in numlist)
-            {
-                Console.WriteLine(i);
-            }
-            numlist.RemoveAt(0);
-
-            List<Point> pList = new List<Point>();
-            pList.Add(p1);
-            pList.Add(p2);
+            HorizontalLine line = new HorizontalLine(5, 50, 8, '~');
+            line.Draw();
 
             Console.ReadLine();
         }
