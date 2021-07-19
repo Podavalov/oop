948a34a7cb728b2fcddc45c336616a35d797c2fd
Gleb <podavalovg@gmail.com>
2021-07-19 20:10:21 +0500

    step 12

--- Snake/Snake/Program.cs
+++ Snake/Snake/Program.cs
@@ -12,35 +12,52 @@ namespace Snake
         static void Main(string[] args)
         {
 
-            VerticalLine v1 = new VerticalLine(0, 10, 5, '%');
-            Draw(v1);
-
-            Point p = new Point(10, 8, '#');
-            Figure fSnake = new Snake(p, 4, Direction.Right);
-            Draw(fSnake);
-            Snake snake = (Snake)fSnake;
-            HorizontalLine h1 = new HorizontalLine(0, 5, 6, '&');
-
-            List<Figure> figures = new List<Figure>();
-            figures.Add(fSnake);
-            figures.Add(v1);
-            figures.Add(h1);
-
-            foreach(var f in figures)
-            {
-                f.Draw();
-            }
-
-          
-
-
            
+
+            Walls walls = new Walls(80, 20);
+            walls.Draw();
+
             
-        }
-  static void Draw(Figure figure)
+
+            Point p = new Point(15, 15, '#');
+            Snake snake = new Snake(p, 4, Direction.Right);
+            snake.Draw();
+
+            FoodCreator foodCreator = new FoodCreator(80, 20, '$');
+            Point food = foodCreator.CreateFood();
+            food.Draw();
+           
+
+            while (true)
             {
-                figure.Draw();
+                if (walls.IsHit(snake) || snake.IsHitTail())
+                {
+                    break;
+
+                }
+
+                if (snake.Eat(food))
+                {
+                    food = foodCreator.CreateFood();
+                    food.Draw();
+                }
+                else
+                {
+                    snake.Move();
+                }
+                Thread.Sleep(100);
+
+                if (Console.KeyAvailable)
+                {
+                    ConsoleKeyInfo key = Console.ReadKey();
+                    snake.HandlKey(key.Key);
+                }
             }
+           
+        }
+            
+    }
+    
        
     }
-}
+
---
+++ Snake/Snake/Walls.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Snake
+{
+    class Walls:Figure
+    {
+        List<Figure> wallList;
+        public Walls(int mapWidth, int mapHeight)
+        {
+            wallList = new List<Figure>();
+            HorizontalLine UpLine = new HorizontalLine(0, mapWidth - 2, 0, '+');
+            HorizontalLine DownLine = new HorizontalLine(0, mapWidth -2, mapHeight - 1, '+');
+            VerticalLine Leftline = new VerticalLine(0, mapHeight -1 , 0, '+');
+            VerticalLine Rightline = new VerticalLine(0, mapHeight-1, mapWidth -2, '+');
+
+            wallList.Add(UpLine);
+            wallList.Add(DownLine);
+            wallList.Add(Leftline);
+            wallList.Add(Rightline);
+        }
+        internal bool IsHit (Figure figure)
+        {
+            foreach (var wall in wallList)
+            {
+                if (wall.IsHit(figure))
+                {
+                    return true;
+                }
+                
+            }
+         return false;
+        } 
+        public void Draw()
+            {
+            foreach (var wall in wallList)
+            {
+                wall.Draw();
+            } 
+            
+            }
+    }
+}
