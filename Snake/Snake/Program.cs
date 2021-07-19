0014bc2087759ee073065474efbd3b88cd075eb8
Gleb <podavalovg@gmail.com>
2021-07-19 06:15:00 +0500

    step5

--- Snake/Snake/Program.cs
+++ Snake/Snake/Program.cs
@@ -10,14 +10,19 @@ namespace Snake
     {
         static void Main(string[] args)
         {
-            Point p1 = new Point(1, 3, '*');
-            p1.Draw(); 
-           
-             Point p2 = new Point(2, 3, '#');
-                   p2.Draw();
 
-            HorizontalLine line = new HorizontalLine(5, 50, 8, '~');
-            line.Draw();
+            Console.SetWindowSize(90, 30);
+            HorizontalLine UpLine = new HorizontalLine(0, 85, 0, '+');
+            HorizontalLine DownLine = new HorizontalLine(0, 85, 25, '+');
+            VerticalLine Leftline = new VerticalLine(0, 25, 0, '+');
+            VerticalLine Rightline = new VerticalLine(0, 25, 85, '+');
+            UpLine.Draw();
+            DownLine.Draw();
+            Leftline.Draw();
+            Rightline.Draw();
+
+            Point p = new Point(10, 8, '*');
+            p.Draw();
 
             Console.ReadLine();
         }
---
+++ Snake/Snake/VerticalLine.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Snake
+{
+    class VerticalLine
+    {
+        List<Point> pList;
+
+        public VerticalLine(int yMin, int yMax, int x, char sym )
+        {
+            pList = new List<Point>();
+
+            for (int y = yMin; y <= yMax; y++)
+            {
+                Point p = new Point(x, y, sym);
+                pList.Add(p);
+            }
+        }
+        public void Draw()
+        {
+            foreach (Point p in pList)
+            {
+                p.Draw();
+            }
+        }
+
+
+    }
+}
